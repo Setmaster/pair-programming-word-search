9474df8406643710153c7311fcb39c10e79ecb31
Vitor <vi7org@hotmail.com>
2024-10-26 19:43:38 -0400

    implement vertical word search

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,20 @@
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
+    
+    // for each column index
+    const verticalJoin = letters[0].map((_, colIndex) =>
+        // get the letter at that index for each row into an array then join them
+        letters.map(row => row[colIndex]).join('')
+    );
+    
+    for (let l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+    
+    for (let l of verticalJoin){
+        if (l.includes(word)) return true;
+    }
+    return false;
 }
 
 module.exports = wordSearch
